adad3f2f99f9970035f37dfdec9aac9f1ca61f70
Lisa Wells <lisawells999@gmail.com>
2017-11-03 08:44:26 -0400

    enqueue parent style

--- functions.php
+++ functions.php
@@ -1,5 +1,20 @@
 <?php
 
+function statorec_enqueue_styles() {
+
+    $parent_style = 'typology-main'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/assets/css/min.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action( 'wp_enqueue_scripts', 'statorec_enqueue_styles' );
+
+
+
 /**
  * Get links for co-authors plus plugin, based on typology to get button
  *
