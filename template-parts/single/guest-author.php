84d1d3d8d5967f585e9913d597d1d6addb6d2e24
Lisa Wells <lisawells999@gmail.com>
2017-12-15 18:06:07 -0500

    coauthor view all: display flex for vertically-cntr text

--- template-parts/single/guest-author.php
+++ template-parts/single/guest-author.php
@@ -48,6 +48,7 @@
 
 									<!-- <?php //echo '<div><a class="typology-button-social hover-on" href="/author/'.$nice.'">View All by '.$display_name.'</a></div>'; ?> -->
 									<?php echo '<div><a class="typology-button-social hover-on" href="/author/'.$nice.'">View All by '.$display_name.'</a>'; ?>
+									<div class="coauthor-social">
 									<?php if( $coauthor_facebook ) { echo '<a href="'.$coauthor_facebook.'" target="_blank" class="typology-icon-social hover-on fa fa-facebook"></a>'; } ?>
 									<?php if( $coauthor_twitter ) { echo '<a href="'.$coauthor_twitter.'" target="_blank" class="typology-icon-social hover-on fa fa-twitter"></a>'; } ?>
 									<?php if( $coauthor_instagram ) { echo '<a href="'.$coauthor_instagram.'" target="_blank" class="typology-icon-social hover-on fa fa-instagram"></a>'; } ?>
@@ -56,6 +57,7 @@
 									} ?>
 									<!-- .typology-icon-social -->
 									<?php echo '</div>'; ?>
+									</div>
 
 								</div>
 
