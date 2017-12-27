e60a153d9b0a50acbc66036d025b0e7c6b83182d
Lisa Wells <lisawells999@gmail.com>
2017-12-27 11:59:20 -0500

    add byline and footnote formats and styles

--- functions.php
+++ functions.php
@@ -94,4 +94,42 @@ function statorec_mce_buttons_2($buttons) {
 }
 add_filter('mce_buttons_2', 'statorec_mce_buttons_2');
 
+/*
+* Callback function to filter the MCE settings
+*/
+function my_mce_before_init_insert_formats( $init_array ) {
+
+// Define the style_formats array
+
+    $style_formats = array(
+/*
+* Each array child is a format with it's own settings
+* Notice that each array has title, block, classes, and wrapper arguments
+* Title is the label which will be visible in Formats menu
+* Block defines whether it is a span, div, selector, or inline style
+* Classes allows you to define CSS classes
+* Wrapper whether or not to add a new block-level element around any selected elements
+*/
+        array(
+            'title' => 'Byline',
+            'block' => 'p',
+            'classes' => 'byline',
+            // 'wrapper' => true,
+        ),
+        array(
+            'title' => 'Footnote',
+            'block' => 'p',
+            'classes' => 'footnote',
+            // 'wrapper' => true,
+        ),
+    );
+    // Insert the array, JSON ENCODED, into 'style_formats'
+    $init_array['style_formats'] = json_encode( $style_formats );
+
+    return $init_array;
+
+}
+// Attach callback to 'tiny_mce_before_init'
+add_filter( 'tiny_mce_before_init', 'my_mce_before_init_insert_formats' );
+
 ?>
